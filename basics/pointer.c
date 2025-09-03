ff57deed0233ae20467113a7f852817c2d1096c6
cesarmars <cesar.marsfall@gmail.com>
2025-09-02 20:35:41 -0700

    Updated integer pointers

--- basics/pointer.c
+++ basics/pointer.c
@@ -10,9 +10,24 @@ int main () {
 	// gives the variable an address
 	// keeps it in memory.
 	addOne(&value); 
-
 	// prints out the value 11
 	printf("Value: %d\n", value);
+
+	int x = 2; // int variable
+	int *pX;  // pointer 
+	pX = &x; // pointer that stores the value 2 has an address
+
+	printf("Value of x: %d\n", x); // prints out the value 2
+	printf("The address of x: %p\n", &x); // address of x
+	printf("The address via pointer -> pX: %p\n", pX); // the same as above (pX = &x)
+	printf("Value of x via pointer -> *pX: %d\n", *pX); // prints out the value 2
+
+	*pX = *pX + 2; // updates (int value) and (int *pX) to 2 + 2 = 4
+
+	printf("Value of new x: %d\n", x); // prints out the value 4
+	printf("Value of new x via pointer -> *pX: %d\n", *pX); // prints out the value 4
+	printf("The address of x: %p\n", &x); // same address
+	printf("The address of x via pointer -> pX: %p\n", pX); // same address
 
 	return 0;
 
