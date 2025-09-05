296c6d0c20038030e5ad9261c7613caf087a5ec2
cesarmars <cesar.marsfall@gmail.com>
2025-09-05 11:49:10 -0700

    Command-line argument

---
+++ basics/argc-argv.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+	printf("Number of argc strings: %d\n", argc); // number of strings on the command line
+
+	for(int i = 0 ; i <= argc; i++) {
+		// argv is a pointer variable that points to each element in the array of strings
+		// array -> ["./a.out", null]
+		printf("Argc number %d: %s\n", i, argv[i]);  
+	}	
+	return 0;
+
+}
